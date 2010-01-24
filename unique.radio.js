556746f20bcc900935366a27e99f44639c51b4a1
John <john@theworkinggroup.ca>
2010-01-24 13:03:08 +0000

    init

---
+++ unique.radio.js
@@ -0,0 +1,21 @@
+/* 
+ * patch to allow unique radio buttons inside a .NET repeater
+ * ..use code-behind to bind it to the controls
+ */
+
+function setUniqueRadioRepeater(nameregex, current)
+{
+   re = new RegExp(nameregex);
+   for(i = 0; i < document.forms[0].elements.length; i++)
+   {
+      elm = document.forms[0].elements[i]
+      if (elm.type == 'radio')
+      {
+         if (re.test(elm.name))
+         {
+            elm.checked = false;
+         }
+      }
+   }
+   current.checked = true;
+}
